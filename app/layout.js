bc23f003b3e4e43326b1acc6274d88439b82e8a5
isoppoeletro-super-vendas <isoppoeletro@gmail.com>
2026-05-14 08:49:06 -0300

    fix build layout pixel

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MetaPixel from "./components/MetaPixel";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Encosto Massageador",
@@ -19,11 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="pt-BR"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
+    <html lang="pt-BR">
+      <body>
         <MetaPixel />
         {children}
       </body>
